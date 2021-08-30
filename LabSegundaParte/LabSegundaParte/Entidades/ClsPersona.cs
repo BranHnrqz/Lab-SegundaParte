8aad13e8ccba8191d4f1f06da795fea6ceb594e5
rodrigo2003 <rs0212032021@unab.edu.sv>
2021-08-30 15:13:08 -0600

    SegundoCommir

--- LabSegundaParte/LabSegundaParte/Entidades/ClsPersona.cs
+++ LabSegundaParte/LabSegundaParte/Entidades/ClsPersona.cs
@@ -8,5 +8,16 @@ namespace LabSegundaParte.Entidades
 {
     class ClsPersona
     {
+        private string nombre;
+        private int edad;
+        private string sexo;
+        private double peso;
+        private double altura;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public double Peso { get => peso; set => peso = value; }
+        public double Altura { get => altura; set => altura = value; }
     }
 }
