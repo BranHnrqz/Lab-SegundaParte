5def364dbf982c711f4174d4a9b30fe46d59a27a
BranHnrqz <86216647+BranHnrqz@users.noreply.github.com>
2021-08-30 16:08:19 -0600

    Update

--- LabSegundaParte/LabSegundaParte/Program.cs
+++ LabSegundaParte/LabSegundaParte/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LabSegundaParte.Entidades;
+using LabSegundaParte.Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,8 @@ namespace LabSegundaParte
     {
         static void Main(string[] args)
         {
+            ClsPersona clspersona = new ClsPersona();
+            NClsPersona nclspersona = new NClsPersona();
         }
     }
 }
